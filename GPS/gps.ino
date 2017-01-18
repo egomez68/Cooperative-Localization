a8357e092dd87f9f481eb7b6605ac243eba430b6
egomez68 <e-gomez@onu.edu>
2017-01-18 18:26:31 -0500

    Update gps.ino

--- GPS/gps.ino
+++ GPS/gps.ino
@@ -1,2 +1,36 @@
 // GPS module should provide functions that parse through a received NMEA sentence
 // and extract relevant information. (Such as latitude and longitude.)
+
+#include <TinyGPS++.h>
+
+// Assign pins to connect gps to
+int RXPin; // add value for pin
+int TXPin; // add value for pin
+
+// Instantiate TinyGPS++ object named "gps"
+TinyGPSPlus gps;
+
+// Instantiate integer with GPS baud rate I
+int GPSBaud; // add value for baud rate
+
+void setup()
+{
+  // Set baud rate for GPS serial communications
+  Serial1.begin(GPSBaud);   
+}
+
+void loop()
+{
+  while (Serial1.available() > 0) // check for GPS data
+  {
+    if (gps.encode(Serial1.read())) // encode GPS data
+    {
+      float gpsLng,gpsLat;
+      gpsLng = gps.location.lng(); // get longitude
+      gpslat = gps.location.lat(); // get latitude
+    }
+  }
+}
+      
+      
+      
