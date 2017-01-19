2850d6d7a77ce0cf63a6944fbd170e979df07b0b
egomez68 <e-gomez@onu.edu>
2017-01-19 15:12:47 -0500

    Update xbeeBroadcast.ino

--- XBEE/xbeeBroadcast.ino
+++ XBEE/xbeeBroadcast.ino
@@ -1,1 +1,34 @@
 // This module should broadcast gps (psuedo)errors.
+
+#include <EasyTransfer.h>
+
+// create EasyTransfer object
+EasyTransfer ET;
+
+struct SEND_DATA_STRUCTURE
+{
+  float errorLat;
+  float errorLng;
+  float timestamp;
+}
+
+// name the group of data contained in structure
+SEND_DATA_STRUCTURE nodeData;
+
+void setup()
+{
+  Serial2.begin(9600);
+  // start EasyTrasfer on Serial2
+  ET.begin(details(nodeData),&Serial2);
+}
+
+void loop()
+{
+  // must initialize these variables with respective values
+  nodeData.errorLat = 
+  nodeData.errorLng = 
+  nodeData.timestamp = 
+
+  // after the structure is created, we send the data
+  ET.sendData();
+}
