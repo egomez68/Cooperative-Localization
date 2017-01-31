15168cae496989cc9b78db2ca2105fbf29e78959
k-lanquist <k-lanquist@onu.edu>
2017-01-31 14:26:34 -0500

    Add files via upload

---
+++ UTCTestSender.ino
@@ -0,0 +1,52 @@
+#include <SD.h>
+#include <SPI.h>
+
+/*
+ * miso -  50(digital)
+ * scl - 52(digital)
+ * mos1 - 51(digital)
+ * cs - 53(digital)
+ */
+
+int CS_PIN = 53;
+
+File file;
+char incomingByte;
+char str[2];
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial.write(1);
+
+  Serial1.begin(9600);
+
+ //  Serial.write(1);
+  // delay(10000);
+  // Serial.write(1);
+   
+}
+
+void loop()
+{
+  //while ( Serial1.available() > 0 ) {
+
+   // incomingByte = Serial1.read();
+   // if(incomingByte == '$'){
+    //  Serial.println("");
+    //}
+
+
+ 
+
+  
+  
+    
+  
+  
+}
+
+
+
+
+
