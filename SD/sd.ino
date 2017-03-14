d6c0f7faebd8e4729d1a90a79eb08e78ef38d5e5
egomez68 <e-gomez@onu.edu>
2017-03-14 18:50:37 -0400

    Changed to generic example.
    Demonstrates implementation of SD library and writing to external memory.

--- SD/sd.ino
+++ SD/sd.ino
@@ -1,3 +1,41 @@
-// This module should log the following in a file on the sd card:
-//  1) (Estimated Longitude Error,Estimated Latitude Error)
-//  3) (Estimated Longitude,Estimated Latitude)
+//********** Library Includes **********//
+
+#include <SD.h> // Must include the SD library
+
+//********** Object Instantiation **********//
+
+File myData;  // Instantiate a File object
+
+//********** Variable Declaration **********//
+
+int CS_PIN = 53; // Assign a pin for chip select
+
+void setup(){
+  initializeSD();  // Initialize the SD card reader
+  
+  mySensorData.print("This ");
+  mySensorData.print("Is "); 
+  mySensorData.print("How ");
+  mySensorData.print("You ");
+  mySensorData.print("Write ");
+  mySensorData.print("To ");
+  mySensorData.print("The ");
+  mySensorData.print("SD ");
+  mySensorData.println("Card."); // Notice the use of 'println' in order to start a new line
+  mySensorData.print("Written By: Edgar Gomez");
+  
+  // Output:
+  //    This Is How You Write To The SD Card.
+  //    Written By: Edgar Gomez
+}
+
+void loop(){
+}
+
+void initializeSD(){
+ pinMode(10, OUTPUT); // Must declare 10 an output to reserve it
+ SD.begin(CS_PIN);    // Initialize the SD care reader
+ 
+ if(SD.exists("myFile.txt")) // Delete old data files to start fresh
+    SD.remove("myFile.txt");
+}
