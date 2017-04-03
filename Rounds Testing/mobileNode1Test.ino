994f0fe2660635b77d1f77cc71931b27a3fcc8c7
egomez68 <e-gomez@onu.edu>
2017-04-03 10:16:58 -0400

    Update and rename mobileNode_Rounds_Test.ino to mobileNode1Test.ino

--- Rounds Testing/mobileNode1Test.ino
+++ Rounds Testing/mobileNode1Test.ino
@@ -1,13 +1,13 @@
 /* 
- *  File        : mobileNode2Test.ino
+ *  File        : mobileNode1Test.ino
  *  Description : Implements the R.I.D.E localization algorithm. 
- *                Upload to mobile node 2
+ *                Upload to mobile node 1
  *  Author      : Edgar Gomez, Tim Coulter
  *  Date        : 2 April 2017
  *  Version     : 1.0
  */
  
-/*** Make sure to incoroporate the "beginRide" function - See the RSU ***/
+/*** Make sure to incorporate the "beginRide" function - See the RSU ***/
 
 //********** Library Includes **********//
 
@@ -43,10 +43,10 @@ SEND_DATA_STRUCTURE txdata;
 
 //********** Variable Declaration **********//
 
-bool RSU = false;
-bool mobileNode2 = false;
-bool mobileNode3 = false;
-bool mobileNode4 = false;
+bool RSU = false,
+     mobileNode2 = false,
+     mobileNode3 = false,
+     mobileNode4 = false;
        
 int CS_PIN = 53;
 int F = 0;
@@ -108,12 +108,8 @@ void loop(){
 
     if(RSU && mobileNode2 && mobileNode3 && mobileNode4){
       x_newLat = positionEstimateUpdate(x_prevLat, neighLatError, n_ii, F, "LAT");
-       // Serial.print("myLatError "); Serial.println(myLatError,6);
-       // Serial.print("my new Lat: "); Serial.println(x_newLat,6);
-
       x_newLng = positionEstimateUpdate(x_prevLng, neighLngError, n_ii, F,"");
-        //Serial.print("myLngError "); Serial.println(myLngError,6);
-        //Serial.print("my new Lng: "); Serial.println(x_newLng,6);
+     
       txdata.latErr = myLatError;
       txdata.lngErr = myLngError;
       
@@ -126,7 +122,7 @@ void loop(){
       RSU = false;
       mobileNode2 = false;
       mobileNode3 = false;
-      mobileNode4= false;
+      mobileNode4 = false;
       }
  
     ETout.sendData();
@@ -183,37 +179,27 @@ double positionEstimateUpdate(double x_prev, double* diff_error_set, int n_ii, i
     int kept_neigh_size = uprcount - lwrcount;
   double* kept_neigh = 0;
     kept_neigh = new double[kept_neigh_size];
-    //Serial.print("kept Neigh Size: "); Serial.println(kept_neigh_size);
-    //Serial.print("srtdneigh: "); Serial.println(srtd_neigh[0],6);
-    //Serial.print("lwrcount: "); Serial.println(lwrcount,6);
-
-
-
 
     // Remove up to 2F lowest and 2F highest gps error estimates 
     for (int i = 0; i < kept_neigh_size; i++)
-      kept_neigh[i] = srtd_neigh[i + lwrcount];
-      //Serial.print("kept_neight[i] "); Serial.println(kept_neigh[0],6);
-      
+      kept_neigh[i] = srtd_neigh[i + lwrcount];      
     
     // Calculate number of kept neighbors
     int num_kept = uprcount - lwrcount;
-    //Serial.print("num_kept "); Serial.println(num_kept,6);
-
+   
     // Calculate Weight to Use in Update (m_k)
     double m_k = 1.0 / (num_kept + 1);
-    //Serial.print("m_k "); Serial.println(m_k,6);
 
     // Calculate Sum of Pseudo Errors 
     double sumPseudoErrors = 0;
     for (int i = 0; i<num_kept; i++)
       sumPseudoErrors += kept_neigh[i];
-      //Serial.print("sumPseudoErrors "); Serial.println(sumPseudoErrors,6);
    
     if (type == "LAT") {
      
     // Calculate Leaky-Average of GPS Errors
     myLatError = m_k*sumPseudoErrors;    
+    
     // Calculate position estimate (x_new)
     x_new = x_prev - myLatError;
     }
